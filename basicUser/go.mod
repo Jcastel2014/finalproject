098ab981d2cebb72621e56c9c477c0ac56ad664e
Jcastel2014 <2020253542@ub.edu.bz>
2023-05-08 22:58:02 -0600

    added program 1 from christian hope

---
+++ basicUser/go.mod
@@ -0,0 +1,3 @@
+module github.com/Jcastel018/basicUser
+
+go 1.19
---
+++ basicUser/main.go
@@ -0,0 +1,56 @@
+// Filename: main.go
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/set", setCookieHandler)
+	mux.HandleFunc("/get", getCookieHandler)
+
+	log.Print("Listening...")
+	err := http.ListenAndServe(":3100", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setCookieHandler(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:     "exampleCookie",
+		Value:    "Hello Zoë!",
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+	}
+
+	http.SetCookie(w, &cookie)
+
+	w.Write([]byte("cookie set!"))
+}
+
+func getCookieHandler(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("exampleCookie")
+	if err != nil {
+		switch {
+		case errors.Is(err, http.ErrNoCookie):
+			http.Error(w, "cookie not found", http.StatusBadRequest)
+		default:
+			log.Println(err)
+			http.Error(w, "server error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Write([]byte(cookie.Value))
+}
+
+// curl -i http://localhost:3000/set
+
+// echo "" | base64url --decode
